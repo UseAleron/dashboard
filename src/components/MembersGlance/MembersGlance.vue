edf680b1c6cb206df9854d4ae8d672ab0c2c6f80
Rihan <me@file.properties>
2022-08-27 17:47:17 +0000

    Create member glance component

---
+++ src/components/MembersGlance/MembersGlance.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  members: { id: number; name: string; picture?: string }[];
+}>();
+
+const firstFiveMembers = computed(() => props.members.slice(0, 5));
+</script>
+
+<template>
+  <div class="symbol-group symbol-hover flex-nowrap">
+    <MembersGlanceItem
+      v-for="member in firstFiveMembers"
+      :key="member.id"
+      :name="member.name"
+      :picture="member.picture"
+    />
+    <MembersGlanceOverflow
+      v-if="members.length > 5"
+      :additional="members.length - 5"
+    />
+  </div>
+</template>
---
+++ src/components/MembersGlance/MembersGlanceItem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  picture?: string;
+}>();
+</script>
+
+<template>
+  <div class="symbol symbol-35px symbol-circle">
+    <span
+      v-if="!picture"
+      class="symbol-label bg-warning text-inverse-warning fw-bold"
+    >
+      {{ name[0] }}
+    </span>
+    <img
+      v-else
+      :alt="`${name}'s profile picture`"
+      :src="picture"
+      referrerpolicy="no-referrer"
+    />
+  </div>
+</template>
