84b47921ed245096a525c151005580dc084b0a47
Rihan <me@file.properties>
2022-08-27 18:05:00 +0000

    Create new home view

---
+++ src/components/GroupCard.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import type { Group, Server } from "@/types";
+import GroupServer from "@/components/GroupServer.vue";
+
+defineProps<{ group: Group }>();
+
+const servers = ref<Server[]>([
+  {
+    id: 1,
+    name: "Example's Survival",
+    group: 1,
+    game: "minecraft_java",
+    address: "survival.aleron.game",
+  },
+  {
+    id: 2,
+    name: "TF2",
+    group: 1,
+    game: "team_fortress_2",
+    address: "tf2.aleron.game",
+  },
+  {
+    id: 3,
+    name: "CS:GO",
+    group: 1,
+    game: "counter_strike_global_offence",
+    address: "dust2.aleron.game",
+  },
+  {
+    id: 4,
+    name: "CrazyCraft",
+    group: 1,
+    game: "minecraft_bedrock",
+    address: "bedrock-friends.aleron.game",
+  },
+  {
+    id: 5,
+    name: "Lobby",
+    group: 2,
+    game: "minecraft_java",
+    address: "-",
+  },
+  {
+    id: 6,
+    name: "Factions",
+    group: 2,
+    game: "minecraft_java",
+    address: "-",
+  },
+  {
+    id: 7,
+    name: "Survival",
+    group: 2,
+    game: "minecraft_java",
+    address: "-",
+  },
+  {
+    id: 8,
+    name: "Proxy",
+    group: 2,
+    game: "minecraft_java",
+    address: "blockgamecraft.aleron.game",
+  },
+  {
+    id: 9,
+    name: "Creative",
+    group: 2,
+    game: "minecraft_java",
+    address: "-",
+  },
+]);
+
+const serversByGroup = (group: number) =>
+  servers.value.filter((server) => server.group === group);
+</script>
+
+<template>
+  <div class="card card-xxl-stretch shadow-none">
+    <div class="card-header border-0 pt-5">
+      <h3 class="card-title align-items-start flex-column">
+        <span class="card-label fs-2 mb-1">{{ group.name }}</span>
+      </h3>
+
+      <div class="card-toolbar">
+        <MembersGlance :members="group.members" class="d-none d-sm-block" />
+        <RouterLink
+          :to="{ name: 'home' }"
+          class="btn btn-sm btn-icon btn-outline btn-outline-secondary ms-2 btn-icon-gray-600 btn-active-icon-dark"
+        >
+          <span class="svg-icon svg-icon-2x"><IconCod001 /></span>
+          <span class="d-none">Manage</span>
+        </RouterLink>
+      </div>
+    </div>
+
+    <div class="card-body py-3">
+      <div v-if="serversByGroup(group.id).length" class="row">
+        <div
+          class="col-sm-6 col-xl-4"
+          v-for="server in serversByGroup(group.id)"
+          :key="server.id"
+        >
+          <GroupServer :server="server" />
+        </div>
+      </div>
+
+      <div
+        v-else
+        class="card bg-light border mb-5"
+        style="--bs-border-opacity: 0.2"
+      >
+        <div class="card-body">
+          <h2 class="mb-0 h3 fw-normal">No servers</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,57 +1,64 @@
 <script setup lang="ts">
 import { ref } from "vue";
-
-type GameTypes =
-  | "minecraft_java"
-  | "team_fortress_2"
-  | "counter_strike_global_offence"
-  | "minecraft_bedrock";
-
-interface Group {
-  id: number;
-  name: string;
-}
-
-interface Server {
-  id: number;
-  name: string;
-  group: number;
-  game: GameTypes;
-}
+import type { Group } from "@/types";
 
 const groups = ref<Group[]>([
-  { id: 1, name: "Example's Servers" },
-  { id: 2, name: "BlockGameCraft" },
-  { id: 3, name: "CraftNetwork" },
+  {
+    id: 1,
+    name: "Example's Servers",
+    members: [
+      { id: 1, name: "Dipper", picture: "https://i.imgur.com/t6iTefZ.webp" },
+    ],
+  },
+  {
+    id: 2,
+    name: "BlockGameCraft",
+    members: [
+      {
+        id: 2,
+        name: "Mabel",
+        picture: "https://i.imgur.com/6OTTYEU.png",
+      },
+      {
+        id: 1,
+        name: "Dipper",
+        picture: "https://i.imgur.com/t6iTefZ.webp",
+      },
+      { id: 3, name: "Clyde" },
+      {
+        id: 4,
+        name: "Uncle Stan",
+        picture: "https://i.imgur.com/JRGkWY1.webp",
+      },
+      {
+        id: 5,
+        name: "Waddles",
+        picture: "https://i.imgur.com/mXjJSM1.webp",
+      },
+      {
+        id: 6,
+        name: "Wendy",
+        picture: "https://i.imgur.com/beFwf4d.webp",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "CraftNetwork",
+    members: [
+      {
+        id: 4,
+        name: "Uncle Stan",
+        picture: "https://i.imgur.com/JRGkWY1.webp",
+      },
+      {
+        id: 1,
+        name: "Dipper",
+        picture: "https://i.imgur.com/t6iTefZ.webp",
+      },
+    ],
+  },
 ]);
-
-const servers = ref<Server[]>([
-  { id: 1, name: "Example's Survival", group: 1, game: "minecraft_java" },
-  { id: 2, name: "TF2", group: 1, game: "team_fortress_2" },
-  { id: 3, name: "CS:GO", group: 1, game: "counter_strike_global_offence" },
-  { id: 4, name: "CrazyCraft", group: 1, game: "minecraft_bedrock" },
-  { id: 5, name: "Lobby", group: 2, game: "minecraft_java" },
-  { id: 6, name: "Factions", group: 2, game: "minecraft_java" },
-  { id: 7, name: "Survival", group: 2, game: "minecraft_java" },
-  { id: 8, name: "Proxy", group: 2, game: "minecraft_java" },
-  { id: 9, name: "Proxy", group: 2, game: "minecraft_java" },
-]);
-
-const serversByGroup = (group: number) =>
-  servers.value.filter((server) => server.group === group);
-
-const iconForGame = (game: GameTypes): URL => {
-  switch (game) {
-    case "minecraft_java":
-      return new URL("https://i.imgur.com/08bNwlo.webp", import.meta.url);
-    case "minecraft_bedrock":
-      return new URL("https://i.imgur.com/Pae6fsF.webp");
-    case "team_fortress_2":
-      return new URL("https://i.imgur.com/vtINeD2.webp");
-    case "counter_strike_global_offence":
-      return new URL("https://i.imgur.com/k2dvph3.webp");
-  }
-};
 </script>
 
 <template>
@@ -77,64 +84,10 @@ const iconForGame = (game: GameTypes): URL => {
     </div>
   </PageHeading>
 
-  <div
-    class="card card-xxl-stretch mb-5 mb-xl-8"
+  <GroupCard
     v-for="group in groups"
     :key="group.id"
-  >
-    <div class="card-header border-0 pt-5">
-      <h3 class="card-title align-items-start flex-column">
-        <span class="card-label fs-2 mb-1">{{ group.name }}</span>
-      </h3>
-
-      <div class="card-toolbar">
-        <a href="#" class="btn btn-sm btn-light btn-active-primary">Create</a>
-      </div>
-    </div>
-
-    <div class="card-body py-3">
-      <div v-if="serversByGroup(group.id).length" class="row">
-        <div
-          class="col-md-6 col-lg-4"
-          v-for="server in serversByGroup(group.id)"
-          :key="server.id"
-        >
-          <div class="card mb-5 bg-dark bg-opacity-5">
-            <div class="card-body p-3">
-              <div class="d-flex align-items-center">
-                <div class="symbol symbol-50px me-3">
-                  <span class="symbol-label bg-dark bg-opacity-5">
-                    <img
-                      :src="iconForGame(server.game).toString()"
-                      class="h-75 align-self-center"
-                      :alt="`${server.game} logo`"
-                      referrerpolicy="no-referrer"
-                    />
-                  </span>
-                </div>
-                <div class="d-flex justify-content-start flex-column">
-                  <a href="#" class="text-dark fw-bold text-hover-primary fs-2">
-                    {{ server.name }}
-                  </a>
-                  <span class="text-muted fw-semibold text-muted d-block fs-7">
-                    {{ server.game }}
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        v-else
-        class="card bg-light border mb-5"
-        style="--bs-border-opacity: 0.2"
-      >
-        <div class="card-body">
-          <h2 class="mb-0 h3 fw-normal">No servers</h2>
-        </div>
-      </div>
-    </div>
-  </div>
+    :group="group"
+    class="mb-5 mb-xl-8"
+  />
 </template>
