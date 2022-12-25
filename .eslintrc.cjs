f380e7d4713ad5d1c8ba465a7e21120089833b03
Rihan <me@file.properties>
2022-12-25 02:59:46 +0000

    Update linting config for Nuxt 3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,6 @@
-/* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
   root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
-  ],
-  overrides: [
-    {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
-    },
-  ],
+  extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
   rules: {
     "no-undef": "off",
   },
